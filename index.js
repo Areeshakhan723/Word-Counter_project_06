b6b5a0ee5bd6be0e4bd06ed204dd3f7cd6702a06
Areesha  Khan <160509293+Areeshakhan723@users.noreply.github.com>
2024-04-18 17:50:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        message: "Enter your sentence to count the words",
+        type: "input",
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence words count is: ${words.length}`);
